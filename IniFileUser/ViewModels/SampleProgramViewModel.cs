da534d79dc515ff31aa57835a8c74787823b1e4e
t13801206 <t13801206@yahoo.co.jp>
2021-10-02 19:02:07 +0900

    sample program works good.

---
+++ IniFileUser/ViewModels/SampleProgramViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using System.Collections.Generic;
+using System.Windows.Data;
+
+namespace IniFileUser.ViewModels
+{
+    // https://www.sach.codes/c%23/wpf/mvvm/collectionview/filtering/2019/12/19/Filter-Data-Using-CollectionView.html
+
+    public class SampleProgramViewModel : BindableBase
+    {
+        public ReactiveCommand FilterButtonClicked { get; }
+
+        public ReactiveProperty<string> TbFrom { get; } = new ReactiveProperty<string>();
+
+        public ReactiveProperty<string> TbTo { get; } = new ReactiveProperty<string>();
+
+        public List<Contact> Contacts { get; }
+
+        public CollectionView ContactFilterView { get; }
+
+        public SampleProgramViewModel()
+        {
+            FilterButtonClicked = new ReactiveCommand().WithSubscribe(FilterData);
+            Contacts = GetContacts();
+            ContactFilterView = (CollectionView)CollectionViewSource.GetDefaultView(Contacts);
+            ContactFilterView.Filter = OnFilterTriggered;
+        }
+
+        private void FilterData()
+        {
+            CollectionViewSource.GetDefaultView(Contacts).Refresh();
+        }
+
+        private bool OnFilterTriggered(object item)
+        {
+            if (item is Contact contact)
+            {
+                var bFrom = int.TryParse(TbFrom.Value, out int from);
+                var bTo = int.TryParse(TbTo.Value, out int to);
+
+                if (bFrom && bTo)
+                    return contact.Age >= from && contact.Age <= to;
+            }
+
+            return true;
+        }
+
+        private List<Contact> GetContacts()
+        {
+            return new List<Contact>
+            {
+                new Contact() { Age = 33, Name = "Chelsea" },
+                new Contact() { Age = 30, Name = "Taylor" },
+                new Contact() { Age = 35, Name = "Chris" },
+                new Contact() { Age = 23, Name = "Scarlett" },
+                new Contact() { Age = 42, Name = "Dwayne" },
+            };
+        }
+    }
+
+    public class Contact
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ IniFileUser/Views/SampleProgram.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace IniFileUser.Views
+{
+    /// <summary>
+    /// Interaction logic for SampleProgram.xaml
+    /// </summary>
+    public partial class SampleProgram : Window
+    {
+        public SampleProgram()
+        {
+            InitializeComponent();
+        }
+    }
+}
